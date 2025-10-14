4a7c9484da2d01a65ebcf6882e675a0cd7b26673
Tina <a03993a@gmail.com>
2025-10-14 17:05:22 +0800

    feat: add 002 Add Two Numbers

---
+++ codes/002_add_two_numbers.js
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+var addTwoNumbers = function(l1, l2) {
+    let carry = 0;
+    let dummyHead = new ListNode(0);
+    let curr = dummyHead;
+
+    while (l1 || l2 || carry > 0) {
+        let val1 = l1 ? l1.val : 0
+        let val2 = l2 ? l2.val : 0
+
+        sum = val1 + val2 + carry
+        carry = Math.floor(sum / 10)
+
+        let newNode = new ListNode(sum % 10)
+
+        curr.next = newNode;
+        curr = curr.next;
+
+        l1 = l1?.next;
+        l2 = l2?.next;
+    }
+
+    return dummyHead.next;
+};
