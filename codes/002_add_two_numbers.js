d14b5065a4eafed7d48fafa56241742c7f8866a4
Tina <a03993a@gmail.com>
2025-10-16 18:09:12 +0800

    refactor: update code after review for 002 Add Two Numbers

--- codes/002_add_two_numbers.js
+++ codes/002_add_two_numbers.js
@@ -16,13 +16,13 @@ var addTwoNumbers = function(l1, l2) {
     let carry = 0;
 
     while (l1 || l2 || carry > 0) {
-        let val1 = l1 ? l1.val : 0
-        let val2 = l2 ? l2.val : 0
+        const val1 = l1 ? l1.val : 0;
+        const val2 = l2 ? l2.val : 0;
 
-        sum = val1 + val2 + carry
-        carry = Math.floor(sum / 10)
+        const sum = val1 + val2 + carry;
+        carry = Math.floor(sum / 10);
 
-        let newNode = new ListNode(sum % 10)
+        const newNode = new ListNode(sum % 10);
 
         curr.next = newNode;
         curr = curr.next;
