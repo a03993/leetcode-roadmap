460bebfbe74771cc1e5dfd06fa6d168bde52e32f
Tina <a03993@yahoo.com.tw>
2025-11-02 17:47:58 +0800

    feat: add 092 Reverse Linked List II
    - revise note for 206 after review
    - update script to ensure "II" prints correctly

--- scripts/update-readme.js
+++ scripts/update-readme.js
@@ -25,7 +25,7 @@ codeFiles.forEach((file) => {
 
     const title = problemSlug
         .split("_")
-        .map((s) => s.charAt(0).toUpperCase() + s.slice(1))
+        .map((s) => (s == s.toUpperCase() ? s : s.charAt(0).toUpperCase() + s.slice(1)))
         .join(" ");
 
     const codeLink = `[Link](codes/${file})`;
