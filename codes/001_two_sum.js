132ac8e83a3a06ee8943a91cb8111ec60f2f58cb
Tina <a03993a@gmail.com>
2025-11-17 11:11:08 +0800

    feat: add 167 Two Sum II - Input Array Is Sorted
    - review 001 Two Sum
    - update script to ensure "-" prints correctly

--- codes/001_two_sum.js
+++ codes/001_two_sum.js
@@ -7,8 +7,10 @@ var twoSum = function (nums, target) {
     const map = new Map();
 
     for (let i = 0; i < nums.length; i++) {
-        if (map.has(target - nums[i])) {
-            return [map.get(target - nums[i]), i];
+        const complement = target - nums[i];
+
+        if (map.has(complement)) {
+            return [map.get(complement), i];
         }
 
         map.set(nums[i], i);
